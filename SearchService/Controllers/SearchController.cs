d843724e602b33287c113854fc1e8fa01c12f744
Joabe Rochadel <joaberochadel@gmail.com>
2024-07-19 14:29:02 -0300

    add search searchservice

---
+++ SearchService/Controllers/SearchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    public class SearchController : ControllerBase
+    {
+        //[HttpGet]
+        //public async Task<ActionResult<Item>>
+    }
+}
